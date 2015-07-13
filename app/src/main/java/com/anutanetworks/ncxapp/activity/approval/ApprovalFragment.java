cec44917a241b1becd069986c217daf1bb03e6a3
aditi8 <aditi.nagar2@gmail.com>
2015-07-13 00:49:30 -0700

    test

--- app/src/main/java/com/anutanetworks/ncxapp/activity/approval/ApprovalFragment.java
+++ app/src/main/java/com/anutanetworks/ncxapp/activity/approval/ApprovalFragment.java
@@ -68,8 +68,6 @@ public class ApprovalFragment extends Fragment implements AbsListView.OnItemClic
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
